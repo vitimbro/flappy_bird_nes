648b8b0297d84e02a69abb7b33278064a963eca1
Vitor Fernandes <113260172+vitimbro@users.noreply.github.com>
2024-12-08 11:44:02 -0300

    Some comments

--- flappy_bird.c
+++ flappy_bird.c
@@ -1,45 +1,110 @@
+//--------------------------------------------------------//
+//               Flappy Bird for the NES                  //
+//--------------------------------------------------------//
+// ~ by vitimbro and catsyser
 
+/*
+
+                     ▄███████████████▄
+                   ▄████░░░░░░░░█▀    █▄    
+                  ██░░░░░░░░░░░█▀      █▄
+                 ██░░░░░░░░░░░█▀        █▄
+                ██░░░░░░░░░░░░█          ██
+               ██░░░░░░░░░░░░░█      ██  ██
+              ██░░░░░░░░░░░░░░█▄     ██  ██
+             ████████████░░░░░░██        ██
+            ██░░░░░░░░░░░██░░░░░█████████████
+            ██░░░░░░░░░░░██░░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓█
+            ██░░░░░░░░░░░██░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
+             ▀███████████▒▒▒▒█▓▓▓███████████▀
+                ██▒▒▒▒▒▒▒▒▒▒▒▒█▓▓▓▓▓▓▓▓▓▓▓▓█
+                 ██▒▒▒▒▒▒▒▒▒▒▒▒██▓▓▓▓▓▓▓▓▓▓█
+                  █████▒▒▒▒▒▒▒▒▒▒██████████
+                     ▀███████████▀
+                     
+*/
+
+
+// Standard Libraries
 #include <stdlib.h>
 #include <string.h>
 
-#include <stdlib.h>
-#include <string.h>
+// NES-Specific Libraries
+#include "neslib.h"                 // NES Library with useful NES functions
+#include <nes.h>                    // CC65 NES Header (PPU definitions)
 
-// include NESLIB header
-#include "neslib.h"
-
-// include CC65 NES Header (PPU)
-#include <nes.h>
+// Arithmetic and VRAM Utilities
+#include "bcd.h"                    // BCD arithmetic support
+//#link "bcd.c"
+#include "vrambuf.h"                // VRAM update buffer
+//#link "vrambuf.c"
 
 // CHR Data
-//#resource "tileset.chr"       // Character set (CHR) data
+//#resource "tileset.chr"           // Character set (CHR) data
 //#link "tileset.s"
 
 // Game Nametables
 #include "nametable_game.h"
 
-// BCD arithmetic support
-#include "bcd.h"
-//#link "bcd.c"
+//--------------------------------------------------------//
+//                 CONSTANTS AND DEFINES                  //
+//--------------------------------------------------------//
 
-// VRAM update buffer
-#include "vrambuf.h"
-//#link "vrambuf.c"
+
+
+
+
+
+
+//--------------------------------------------------------//
+//                     PALETTE SETUP                      //
+//--------------------------------------------------------//
+
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
-  0x21,			// screen color
+  0x21,			            // screen color
 
-  0x19,0x29,0x39,0x00,	// background palette 0
-  0x19,0x27,0x37,0x00,	// background palette 1
-  0x19,0x2C,0x39,0x00,	// background palette 2
-  0x2C,0x3D,0x30,0x00,   // background palette 3
+  0x19,0x29,0x39,0x00,	            // background palette 0
+  0x19,0x27,0x37,0x00,	            // background palette 1
+  0x19,0x2C,0x39,0x00,	            // background palette 2
+  0x2C,0x3D,0x30,0x00,              // background palette 3
 
-  0x16,0x35,0x24,0x00,	// sprite palette 0
-  0x00,0x37,0x25,0x00,	// sprite palette 1
-  0x0D,0x2D,0x3A,0x00,	// sprite palette 2
-  0x0D,0x27,0x2A	// sprite palette 3
+  0x16,0x35,0x24,0x00,	            // sprite palette 0
+  0x00,0x37,0x25,0x00,	            // sprite palette 1
+  0x0D,0x2D,0x3A,0x00,	            // sprite palette 2
+  0x0D,0x27,0x2A	            // sprite palette 3
 };
+
+
+
+//--------------------------------------------------------//
+//                       VARIABLES                        //
+//--------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------//
+//                  FUNCTION PROTOTYPES                   //
+//--------------------------------------------------------//
+
+
+void setup_graphics();
+
+
+
+
+//--------------------------------------------------------//
+//                       FUNCTIONS                        //
+//--------------------------------------------------------//
+
 
 // setup PPU and tables
 void setup_graphics() {
@@ -48,6 +113,11 @@ void setup_graphics() {
   // set palette colors
   pal_all(PALETTE);
 }
+
+
+//--------------------------------------------------------//
+//                    MAIN GAME LOOP                      //
+//--------------------------------------------------------//
 
 void main(void)
 {
